3ff3e2bc53592d9994e3d70a1fc4ee6641b99470
Dmitry <khilkodmitry@mail.ru>
2022-02-08 19:52:14 +0300

    Add LoginTest

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class LoginTest {
+    @Test
+    public void login(){
+        open("https://hdn.testrail.io");
+        $(byXpath("//input[@name='name']")).sendKeys("hdn_tms@mail.ru");
+        $(byXpath("//input[@name='password']")).sendKeys("pVui0CaU1AsUDIXrPMws");
+        $(byXpath("//span[@class='loginpage-checkmark']")).click();
+        $(byXpath("//button[@id='button_primary']")).click();
+        $(byXpath("//span[contains(text(),'Dima Hilko')]")).click();
+        $(byXpath("//a[@id='navigation-user-logout']")).click();
+    }
+}
