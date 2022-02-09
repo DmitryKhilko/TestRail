4b060a5d46cf163f8cf53d34a8c81d13a1cfe806
Dmitry <khilkodmitry@mail.ru>
2022-02-09 13:23:00 +0300

    Created a BaseTest and modified the LoginTest

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,20 +1,42 @@
+import base.BaseTest;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class LoginTest {
+public class LoginTest extends BaseTest {
     @Test
-    public void login(){
-        Configuration.browser="edge";
-        open("https://hdn.testrail.io");
-        $(byXpath("//input[@name='name']")).sendKeys("hdn_tms@mail.ru");
-        $(byXpath("//input[@name='password']")).sendKeys("pVui0CaU1AsUDIXrPMws");
-        $(byXpath("//span[@class='loginpage-checkmark']")).click();
-        $(byXpath("//button[@id='button_primary']")).click();
-        $(byXpath("//span[contains(text(),'Dima Hilko')]")).click();
-        $(byXpath("//a[@id='navigation-user-logout']")).click();
+    public void logInlogOutCorrectUsernameCorrectPassword(){
+        open("/auth/login/");
+        $(byXpath("//input[@name='name']")).sendKeys("hdn_tms@mail.ru"); //вводим в поле ввода "Email" email пользователя
+        $(byXpath("//input[@name='password']")).sendKeys("pVui0CaU1AsUDIXrPMws"); //вводим в поле ввода "Password" пароль пользователя
+        $(byXpath("//span[@class='loginpage-checkmark']")).click(); //Снимаем флажок "Keep me logged in"
+        $(byXpath("//button[@id='button_primary']")).click(); // Кликаем по кнопке "Log in"
+        //$(byXpath("//span[contains(text(),'Dima Hilko')]")).shouldBe(visible, Duration.ofSeconds(20)); // Duration.ofSeconds(20) - установка ожидания появления элемента, отличного от установки в BaseTest (там сейчас 5 секунд). На случай, если какие-то части приложения работают медленно
+        $(byXpath("//span[contains(text(),'Dima Hilko')]")).shouldBe(visible); // Проверка успешности входа в веб-приложение: проверяем наличие пункта меню "Dima Hilko" на открывшейся странице
+
+        $(byXpath("//span[contains(text(),'Dima Hilko')]")).click(); // Кликаем по пункту меню "Dima Hilko"
+        $(byXpath("//a[@id='navigation-user-logout']")).click(); // Кликаем по пункту меню "Logout"
+        $(byXpath("//button[@id='button_primary']")).shouldBe(visible); // Проверка успешности выхода в веб-приложение: проверяем наличие кнопки "Log in" на открывшейся странице
     }
+
+//    @Test
+//    public void logout(){
+//        open("/auth/login/");
+//        $(byXpath("//input[@name='name']")).sendKeys("hdn_tms@mail.ru"); //вводим в поле ввода "Email" email пользователя
+//        $(byXpath("//input[@name='password']")).sendKeys("pVui0CaU1AsUDIXrPMws"); //вводим в поле ввода "Password" пароль пользователя
+//        $(byXpath("//span[@class='loginpage-checkmark']")).click(); //Снимаем флажок "Keep me logged in"
+//        $(byXpath("//button[@id='button_primary']")).click(); // Кликаем по кнопке "Log in"
+//        $(byXpath("//span[contains(text(),'Dima Hilko')]")).shouldBe(visible); // Проверка успешности входа в веб-приложение: проверяем наличие пункта меню "Dima Hilko" на открывшейся странице
+//
+//        $(byXpath("//span[contains(text(),'Dima Hilko')]")).click(); // Кликаем по пункту меню "Dima Hilko"
+//        $(byXpath("//a[@id='navigation-user-logout']")).click(); // Кликаем по пункту меню "Logout"
+//        $(byXpath("//button[@id='button_primary']")).shouldBe(visible); // Проверка успешности вsхода в веб-приложение: проверяем наличие кнопки "Log in" на открывшейся странице
+//    }
 }
---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,25 @@
+package base;
+
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class BaseTest {
+    @BeforeMethod
+    public void setUp(){
+        Configuration.browser="firefox";
+        //Configuration.browser="edge";
+        //Configuration.browser="chrome";
+        //Configuration.headless = true; // браузер запускается без UI. Тесты ускоряются и становятся более стабильными. Браузер использует меньше ОЗУ (где-то в 3 раза). Этот режим просто необходим при параллелоьном запуске тестов
+        Configuration.browserSize="1920x1080";
+        Configuration.baseUrl="https://hdn.testrail.io/index.php?"; // Настройка важна, чтобы быстро переключаться на разные окружения (test, prod и т.п.)
+        Configuration.timeout = 5000; // Неявное ожидание в милисекундах (5 секунд), указывающее на то какое максимальное количество времени Selenium будет дожидаться появления элемента (аналог implicitlyWait)
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void tearDown(){
+        getWebDriver().quit();
+    }
+}
