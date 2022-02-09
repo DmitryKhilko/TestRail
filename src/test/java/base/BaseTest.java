ef7834a1e99a064c0d63c0d98ce775b7cd7b9209
Dmitry <khilkodmitry@mail.ru>
2022-02-09 21:32:23 +0300

    Modified the BaseTest

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -9,14 +9,14 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 public class BaseTest {
     @BeforeMethod
     public void setUp(){
-        Configuration.browser="firefox";
+        //Configuration.browser="firefox";
         //Configuration.browser="edge";
         //Configuration.browser="chrome";
         Configuration.headless = true; // браузер запускается без UI. Тесты ускоряются и становятся более стабильными. Браузер использует меньше ОЗУ (где-то в 3 раза). Этот режим просто необходим при параллелоьном запуске тестов
         Configuration.browserSize="1920x1080";
         //Configuration.browserSize="maximized";
         Configuration.baseUrl="https://hdn.testrail.io/index.php?"; // Настройка важна, чтобы быстро переключаться на разные окружения (test, prod и т.п.)
-        Configuration.timeout = 5000; // Неявное ожидание в милисекундах (5 секунд), указывающее на то, какое максимальное количество времени Selenium будет дожидаться появления элемента (аналог implicitlyWait)
+        Configuration.timeout = 10000; // Неявное ожидание в милисекундах (10 секунд), указывающее на то, какое максимальное количество времени Selenium будет дожидаться появления элемента (аналог implicitlyWait)
     }
 
     @AfterMethod (alwaysRun = true)
