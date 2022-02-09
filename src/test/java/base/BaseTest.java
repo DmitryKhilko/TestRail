d8576c68c5093e83ddb2ccad62759987dab325de
Dmitry <khilkodmitry@mail.ru>
2022-02-09 21:37:14 +0300

    Modified the BaseTest

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -10,7 +10,7 @@ public class BaseTest {
     @BeforeMethod
     public void setUp(){
         //Configuration.browser="firefox";
-        //Configuration.browser="edge";
+        Configuration.browser="edge";
         //Configuration.browser="chrome";
         Configuration.headless = true; // браузер запускается без UI. Тесты ускоряются и становятся более стабильными. Браузер использует меньше ОЗУ (где-то в 3 раза). Этот режим просто необходим при параллелоьном запуске тестов
         Configuration.browserSize="1920x1080";
