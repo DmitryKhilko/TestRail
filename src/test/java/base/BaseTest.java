58387b2cb160479f75246933f168a6ac9d3131f3
Dmitry <khilkodmitry@mail.ru>
2022-02-10 02:23:46 +0300

    Modified the BaseTest and the LoginPageTest

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -12,8 +12,7 @@ public class BaseTest {
         //Configuration.browser="firefox";
         //Configuration.browser="edge";
         Configuration.browser="chrome";
-        Configuration.browserVersion="97.0";
-        Configuration.headless = true; // браузер запускается без UI. Тесты ускоряются и становятся более стабильными. Браузер использует меньше ОЗУ (где-то в 3 раза). Этот режим просто необходим при параллелоьном запуске тестов
+        //Configuration.headless = true; // браузер запускается без UI. Тесты ускоряются и становятся более стабильными. Браузер использует меньше ОЗУ (где-то в 3 раза). Этот режим просто необходим при параллелоьном запуске тестов
         Configuration.browserSize="1920x1080";
         Configuration.baseUrl="https://hdn.testrail.io/index.php?"; // Настройка важна, чтобы быстро переключаться на разные окружения (test, prod и т.п.)
         Configuration.timeout = 10000; // Неявное ожидание в милисекундах (10 секунд), указывающее на то, какое максимальное количество времени Selenium будет дожидаться появления элемента (аналог implicitlyWait)
