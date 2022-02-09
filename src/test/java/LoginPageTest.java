20d541ad85009c7b91c19f6dc46ec768e535b3e9
Dmitry <khilkodmitry@mail.ru>
2022-02-10 00:23:12 +0300

    Renamed LoginTest into LoginPageTest

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class LoginTest extends BaseTest {
+public class LoginPageTest extends BaseTest {
     @Test
     public void logInlogOutCorrectUsernameCorrectPassword(){
         open("/auth/login/");
@@ -25,18 +25,4 @@ public class LoginTest extends BaseTest {
         $(byXpath("//a[@id='navigation-user-logout']")).click(); // Кликаем по пункту меню "Logout"
         $(byXpath("//button[@id='button_primary']")).shouldBe(visible); // Проверка успешности выхода в веб-приложение: проверяем наличие кнопки "Log in" на открывшейся странице
     }
-
-//    @Test
-//    public void logout(){
-//        open("/auth/login/");
-//        $(byXpath("//input[@name='name']")).sendKeys("hdn_tms@mail.ru"); //вводим в поле ввода "Email" email пользователя
-//        $(byXpath("//input[@name='password']")).sendKeys("pVui0CaU1AsUDIXrPMws"); //вводим в поле ввода "Password" пароль пользователя
-//        $(byXpath("//span[@class='loginpage-checkmark']")).click(); //Снимаем флажок "Keep me logged in"
-//        $(byXpath("//button[@id='button_primary']")).click(); // Кликаем по кнопке "Log in"
-//        $(byXpath("//span[contains(text(),'Dima Hilko')]")).shouldBe(visible); // Проверка успешности входа в веб-приложение: проверяем наличие пункта меню "Dima Hilko" на открывшейся странице
-//
-//        $(byXpath("//span[contains(text(),'Dima Hilko')]")).click(); // Кликаем по пункту меню "Dima Hilko"
-//        $(byXpath("//a[@id='navigation-user-logout']")).click(); // Кликаем по пункту меню "Logout"
-//        $(byXpath("//button[@id='button_primary']")).shouldBe(visible); // Проверка успешности вsхода в веб-приложение: проверяем наличие кнопки "Log in" на открывшейся странице
-//    }
 }
