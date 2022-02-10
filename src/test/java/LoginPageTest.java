ab4cdeb7ee6dd39496c63a3392a90cea72ec1dab
Dmitry <khilkodmitry@mail.ru>
2022-02-10 11:28:59 +0300

    Modified the BaseTest and the LoginPageTest

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -1,10 +1,5 @@
 import base.BaseTest;
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byXpath;
@@ -12,7 +7,6 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class LoginPageTest extends BaseTest {
-    String mailSplit = "@";
     @Test
     public void logInlogOutCorrectUsernameCorrectPassword(){
         open("/auth/login/");
@@ -26,24 +20,5 @@ public class LoginPageTest extends BaseTest {
         $(byXpath("//span[contains(text(),'Dima Hilko')]")).click(); // Кликаем по пункту меню "Dima Hilko"
         $(byXpath("//a[@id='navigation-user-logout']")).click(); // Кликаем по пункту меню "Logout"
         $(byXpath("//button[@id='button_primary']")).shouldBe(visible); // Проверка успешности выхода в веб-приложение: проверяем наличие кнопки "Log in" на открывшейся странице
-    }
-
-    @Test
-    public void logInlogOutCorrectUsernameCorrectPassword1(){
-        open("/auth/login/");
-        $(byXpath("//input[@name='name']")).sendKeys("hdn_tms"); //вводим в поле ввода "Email" email пользователя
-        $(byXpath("//input[@name='name']")).sendKeys(mailSplit); //вводим в поле ввода "Email" email пользователя
-        $(byXpath("//input[@name='name']")).sendKeys("mail.ru"); //вводим в поле ввода "Email" email пользователя
-
-        $(byXpath("//input[@name='password']")).sendKeys("pVui0CaU1AsUDIXrPMws"); //вводим в поле ввода "Password" пароль пользователя
-        $(byXpath("//span[@class='loginpage-checkmark']")).click(); //Снимаем флажок "Keep me logged in"
-        $(byXpath("//button[@id='button_primary']")).click(); // Кликаем по кнопке "Log in"
-        //$(byXpath("//span[contains(text(),'Dima Hilko')]")).shouldBe(visible, Duration.ofSeconds(20)); // Duration.ofSeconds(20) - установка ожидания появления элемента, отличного от установки в BaseTest (там сейчас 5 секунд). На случай, если какие-то части приложения работают медленно
-        $(byXpath("//span[contains(text(),'Dima Hilko')]")).shouldBe(visible); // Проверка успешности входа в веб-приложение: проверяем наличие пункта меню "Dima Hilko" на открывшейся странице
-
-        $(byXpath("//span[contains(text(),'Dima Hilko')]")).click(); // Кликаем по пункту меню "Dima Hilko"
-        $(byXpath("//a[@id='navigation-user-logout']")).click(); // Кликаем по пункту меню "Logout"
-        $(byXpath("//button[@id='button_primary']")).shouldBe(visible); // Проверка успешности выхода в веб-приложение: проверяем наличие кнопки "Log in" на открывшейся странице
-
     }
 }
