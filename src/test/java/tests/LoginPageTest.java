17bdbb5d21f473e8f15c6ba71298cef08cfb941d
Dmitry <khilkodmitry@mail.ru>
2022-02-11 00:18:59 +0300

    Modified MenuPage (added methods for selecting menu items), modified LoginPageTest

--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -16,7 +16,10 @@ public class LoginPageTest extends BaseTest {
         loginPage.openPage();
         $(LOGIN_BUTTON).shouldBe(visible); // Проверка успешности входа на страницу логина: проверяем наличие кнопки "Log in" на открывшейся странице
         loginPage.login("hdn_tms@mail.ru", "pVui0CaU1AsUDIXrPMws");
-        //$(MENU_USERNAME).shouldBe(visible, Duration.ofSeconds(20)); // Duration.ofSeconds(20) - установка ожидания появления элемента, отличного от установки в BaseTest (там сейчас 5 секунд). На случай, если какие-то части приложения работают медленно
+        //$(MENU_USERNAME).shouldBe(exactText("Dima Hilko"), Duration.ofSeconds(20)); // Duration.ofSeconds(20) - установка ожидания появления элемента, отличного от установки в BaseTest (там сейчас 5 секунд). На случай, если какие-то части приложения работают медленно
         $(MENU_USERNAME).shouldHave(exactText("Dima Hilko")); // Проверка успешности входа в веб-приложение: проверяем наличие пункта меню "Dima Hilko" на открывшейся странице
+
+        menuPage.selectMenuItemLogout(); // выходим из веб-приложения
+        $(LOGIN_BUTTON).shouldBe(visible); // Проверка успешности выхода из веб-приложения: проверяем наличие кнопки "Log in" на открывшейся странице
     }
 }
--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -14,13 +14,13 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp(){
-        //Configuration.browser="firefox";
+        Configuration.browser="firefox";
         //Configuration.browser="edge";
-        Configuration.browser="chrome";
+        //Configuration.browser="chrome";
         //Configuration.headless = true; // браузер запускается без UI. Тесты ускоряются и становятся более стабильными. Браузер использует меньше ОЗУ (где-то в 3 раза). Этот режим просто необходим при параллелоьном запуске тестов
         Configuration.browserSize="1920x1080";
         //Configuration.baseUrl="https://hdn.testrail.io/index.php?"; // Настройка важна, чтобы быстро переключаться на разные окружения (test, prod и т.п.)
-        Configuration.timeout = 10000; // Неявное ожидание в милисекундах (10 секунд), указывающее на то, какое максимальное количество времени Selenium будет дожидаться появления элемента (аналог implicitlyWait)
+        Configuration.timeout = 10000; // неявное ожидание в милисекундах (10 секунд), указывающее на то, какое максимальное количество времени Selenium будет дожидаться появления элемента (аналог implicitlyWait)
 
         //Инициализация страниц, с которыми мы будем работать в тестах
         loginPage = new LoginPage();
